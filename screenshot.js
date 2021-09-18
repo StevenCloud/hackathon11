d2f06abe26a08bb005b2881376ea5c3384a6188e
sebastiandiprampero <sebastiandiprampero@gmail.com>
2021-09-18 16:26:57 -0500

    vdsil

--- screenshot.js
+++ screenshot.js
@@ -77,11 +77,6 @@ DONT FORGET: change screenshot.js to content.js in manifest.json
 
 console.log('content go')
 
-chrome.runtime.onMessage.addListener(gotMessage);
-
-function gotMessage(message, sender, sendResponse) {
-    console.log(message.txt);
-}
 
 document.onmousemove = function(e){
 
