8617e502ba1e0669e5ff806e1218241dcbea1bc0
bucees01 <ar122@rice.edu>
2021-09-18 16:18:02 -0500

    mousing over <a> elements now grabs links

--- content.js
+++ content.js
@@ -11,14 +11,26 @@ var t_links = document.getElementsByTagName('a');
 document.onmousemove = function(e){
     var x = e.pageX;
     var y = e.pageY;
-    e.target.title = "X is "+x+" and Y is " +y;
+    //e.target.title = "X is "+x+" and Y is " +y;
     elementMouseIsOver = document.elementFromPoint(x,y);
 
-console.log(elementMouseIsOver);
+//console.log(elementMouseIsOver);
 };
 
 var MOUSE_VISITED_CLASSNAME = 'crx_mouse_visited';
 
 //document.getElementsByTagName("a").addEventListener('focus', onclick1);
+
+
+
+
+document.addEventListener('mouseover', function(event) {
+    var hoveredl = event.target; // hovered element
+    if (hoveredl.tagName !== 'A') { return; } // ignore non links
+    console.log(hoveredl.href); 
+});
+
+
+
 
 
