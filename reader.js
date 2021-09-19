68819e86454bc486f975cf4b8dd87b2ac3be04f1
sebastiandiprampero <sebastiandiprampero@gmail.com>
2021-09-18 22:13:04 -0500

    hjklgklj

--- reader.js
+++ reader.js
@@ -6,8 +6,9 @@ speech.lang = "en";
 
 voices = window.speechSynthesis.getVoices();
 speech.voice = voices[0];
+var test = 'bruh'
 
 document.addEventListener("click", () => {
-    speech.text = document.querySelector("textarea").value;
+    speech.text = test;
     window.speechSynthesis.speak(speech);
   });
