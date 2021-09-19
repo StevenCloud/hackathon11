ecf6775010e25999c77cb3958b14b37f48c99179
sebastiandiprampero <sebastiandiprampero@gmail.com>
2021-09-18 21:32:13 -0500

    fewfew

--- screenshot.js
+++ screenshot.js
@@ -97,7 +97,7 @@ document.addEventListener('click', function(e){
 fetch("https://api.browshot.com/api/v1/simple?url=http://mobilito.net/&instance_id=12&width=640&height=480&key=my_api_key",
 {mode : 'no-cors'})
 .then(response => {
-    return response();
+    console.log(response);
 })
 .then(json => {
     console.log(json);
