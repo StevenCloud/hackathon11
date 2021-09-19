d6a689a351ae9922fb209277a2f23e329aaa7a88
bucees01 <ar122@rice.edu>
2021-09-19 00:13:44 -0500

    updated dictionary api code

--- alex dictionary/dictAPI/worktest.js
+++ alex dictionary/dictAPI/worktest.js
@@ -1,30 +1,44 @@
-console.log('please work');
+
 
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("click-this").addEventListener("mouseover", setup);
     
 });
-/*
-function handler(){
-    alert('work2?');
-}
-*/
 
 function setup(){
-
+    console.log('setup ok');
     let backpage = chrome.extension.getBackgroundPage();
     let word = backpage.word;
 
     let url = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
     url += word
    
+    var servertext = ''
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            servertxt = xhttp.responseText;
+            message = {
+                text:  xhttp.responseText,
+                id: "dictionary_raw_info",
+            }
+            chrome.runtime.sendMessage(message)
+        }
+    }
+    xhttp.open('GET', url, true);
+    xhttp.send();
+/*
     var request1 = new XMLHttpRequest();
     request1.addEventListener("load",reqListener);
     request1.open("GET", url);
     request1.send();
     
+    window.definition = request1;
+    console.log(window.definition);
     function reqListener(){
         alert(request1.responseText);
     }
+    */
+
     
 }
