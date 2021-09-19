c4cc98fd5c787921b223fbd75af2e3d3459b6828
Steven Cloud <stevencloud9@gmail.com>
2021-09-19 06:36:24 -0500

    sdfsdfsd

--- alex dictionary/content2.js
+++ alex dictionary/content2.js
@@ -1,31 +1,6 @@
-<<<<<<< HEAD
 console.log('ready to go!');
 
-window.addEventListener('mouseup', function mou_up() {
-    console.log('test1');
-    let selected_text = window.getSelection().toString().trim();
-    console.log(selected_text);
-    if (selected_text.length > 0) {
-        let msg = {
-            text: selected_text,
-            id: 'selected_word'
-        };
-        document.getElementById("print_this").innerHTML = selected_text;
-        chrome.runtime.sendMessage(msg)
-        console.log(typeof window.definition);
-    };
-});
-
-/*
-document.addEventListener('mouseover', function(event) {
-    var hoveredl = event.target; // hovered element
-    if (hoveredl.tagName !== 'A') { return; } // ignore non links
-    console.log(hoveredl.href); 
-});
-=======
-console.log('ready to go!');
-
-window.addEventListener('mouseup', function mou_up() {
+window.addEventListener('mouseup', function mou_up() { 
     let selected_text = window.getSelection().toString().trim();
     if (selected_text.length > 0) {
         let msg = {
@@ -41,5 +16,4 @@ document.addEventListener('mouseover', function(event) {
     if (hoveredl.tagName !== 'A') { return; } // ignore non links
     console.log(hoveredl.href); 
 });
->>>>>>> 2af27cd1f275805295bbf133109320b946c2525e
 */
--- alex dictionary/dictAPI/worktest.js
+++ alex dictionary/dictAPI/worktest.js
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("click-this").addEventListener("mouseover", setup);
-    
-});
-
-function setup(){
-    console.log('setup ok');
-    let backpage = chrome.extension.getBackgroundPage();
-    let word = backpage.word;
-
-    let url = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
-    url += word
-   
-    var servertext = ''
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            servertxt = xhttp.responseText;
-            message = {
-                text:  xhttp.responseText,
-                id: "dictionary_raw_info",
-            }
-            chrome.runtime.sendMessage(message)
-        }
-    }
-    xhttp.open('GET', url, true);
-    xhttp.send();
-/*
-    var request1 = new XMLHttpRequest();
-    request1.addEventListener("load",reqListener);
-    request1.open("GET", url);
-    request1.send();
-    
-    window.definition = request1;
-    console.log(window.definition);
-    function reqListener(){
-        alert(request1.responseText);
-    }
-    */
-
-    
-}
-=======
 
 setup();
 
@@ -80,4 +34,3 @@ function messagefinal(request, sender, response) {
         console.log(request.text);
     }
 }
->>>>>>> 2af27cd1f275805295bbf133109320b946c2525e
