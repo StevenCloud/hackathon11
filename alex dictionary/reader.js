681273dafa6e23424a0d9834f233be1564a36360
sebastiandiprampero <sebastiandiprampero@gmail.com>
2021-09-18 23:07:38 -0500

    sdfghj

--- alex dictionary/reader.js
+++ alex dictionary/reader.js
@@ -4,7 +4,7 @@ speech.lang = "en";
 
 voices = window.speechSynthesis.getVoices();
 speech.voice = voices[0];
-var test = 'easy game'
+var test = 'bruh'
 
 document.addEventListener("click", () => {
     speech.text = test;
